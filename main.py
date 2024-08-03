2ce842cdc52ac1d9c3cca144590478f1f4fa8311
Shpetynya <olga.shpetnaja@gmail.com>
2024-08-03 21:23:48 +0300

    task 1 - add .date() to given_date and current_date task 2 - made a selection through .randrange and check quantity in range

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ def get_days_from_today(date):
     '''
     Ця функція розраховує різницю між заданою і поточною датами
     '''
-    try:   
-        given_date = datetime.strptime(date, '%Y-%m-%d')
-        current_date = datetime.today()
+
+    try: 
+        given_date = datetime.strptime(date, '%Y-%m-%d').date()
+        current_date = datetime.today().date()
         difference_days = given_date - current_date
         return difference_days.days
+
     except ValueError:
         print ('Неправильний фомат дати!',)
+      
 
 print(f"{get_days_from_today(date)} days")
 
@@ -27,13 +30,13 @@ def get_numbers_ticket(min_num, max_num, quantity):
     '''
     Функція дозволяє згенерувате потрібне число унікальних чисел для лотереї
     '''
-    if min_num > 0 and max_num <= 1000 and quantity >= min_num and quantity <= max_num:
+    if min_num > 0 and max_num <= 1000 and quantity <= (max_num - min_num):
         unic_numbers = []
         while len(unic_numbers) < quantity:
-            unic_number = random.randint(min_num, max_num)
+            unic_number = random.randrange(min_num, max_num+1)
             if unic_number not in unic_numbers:
                 unic_numbers.append(unic_number)
         return unic_numbers
     else:
-        print('Числа мають бути позитивними та число випадкових чисел має бути у заданому діапазоні')
-print(get_numbers_ticket(1, 49, 20))
+        unic_numbers = []
+print(get_numbers_ticket(10, 14, 4))

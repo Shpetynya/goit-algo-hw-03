24c9f40427c06810dcd4c97ce267f1f38ac5abe1
Shpetynya <olga.shpetnaja@gmail.com>
2024-08-04 21:49:32 +0300

    corrected in task 2

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-# Завдання № 1
-from datetime import datetime
+# # Завдання № 1
+# from datetime import datetime
 
-date = input('Введіть дату у форматі "РРРР-ММ-ДД": ' )
+# date = input('Введіть дату у форматі "РРРР-ММ-ДД": ' )
 
-def get_days_from_today(date):
-    '''
-    Ця функція розраховує різницю між заданою і поточною датами
-    '''
+# def get_days_from_today(date):
+#     '''
+#     Ця функція розраховує різницю між заданою і поточною датами
+#     '''
 
-    try: 
-        given_date = datetime.strptime(date, '%Y-%m-%d').date()
-        current_date = datetime.today().date()
-        difference_days = given_date - current_date
-        return difference_days.days
+#     try: 
+#         given_date = datetime.strptime(date, '%Y-%m-%d').date()
+#         current_date = datetime.today().date()
+#         difference_days = given_date - current_date
+#         return difference_days.days
 
-    except ValueError:
-        print ('Неправильний фомат дати!',)
+#     except ValueError:
+#         print ('Неправильний фомат дати!',)
       
 
-print(f"{get_days_from_today(date)} days")
+# print(f"{get_days_from_today(date)} days")
 
 
 
@@ -35,8 +35,10 @@ def get_numbers_ticket(min_num, max_num, quantity):
         while len(unic_numbers) < quantity:
             unic_number = random.randrange(min_num, max_num+1)
             if unic_number not in unic_numbers:
-                unic_numbers.append(unic_number)
-        return unic_numbers
+                unic_numbers.append(unic_number)   
     else:
         unic_numbers = []
-print(get_numbers_ticket(10, 14, 4))
+
+    return sorted(unic_numbers)
+
+print(get_numbers_ticket(10, 20, 9))
